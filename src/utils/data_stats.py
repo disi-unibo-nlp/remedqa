581d7f41175cb673379efdda27bcfaafdfae699e
Alessio Cocchieri <alessio.cocchieri@outlook.it>
2025-09-17 00:14:19 +0200

    add score for all inconsistency types

--- src/utils/data_stats.py
+++ src/utils/data_stats.py
@@ -4,7 +4,7 @@ import json
 subset = "mmlu"
 model_name = "medgemma"
 input_path = "out/completions/Qwen3-32B-AWQ/judge/mmlu/2025-09-16_07-12-01/judges_mmlu.jsonl" 
-""
+
 with open('data/benchmark.json') as f:
     benchmark = json.load(f)[subset]
 
@@ -32,15 +32,32 @@ print("Accuract Open:", round(accuracy_open, 1))
 consistent_responses = [el for el in data if el['consistent']]
 inconsistent_responses = [el for el in data if not el['consistent']]
 print("Number of consistent responses:", round(len(consistent_responses) / len(data) * 100, 1), "% |", len(consistent_responses))
+print("------")
 print("Number of NON-consistent responses:", round(len(inconsistent_responses) / len(data) * 100, 1), "% |", len(inconsistent_responses))
 
-inconsistent_responses_open_mcq = [el for el in inconsistent_responses if el['correct']]
-print("Number of cases where Open=correct and MCQ=wrong:", len(inconsistent_responses_open_mcq ))
+inconsistent_responses_open_mcq = [el for el in inconsistent_responses if el['correct'] and not el['correct_mcq']]
+print("\twhere Open=correct and MCQ=wrong:", len(inconsistent_responses_open_mcq ))
 
+inconsistent_responses_mcq_open = [el for el in inconsistent_responses if el['correct_mcq'] and not el['correct']]
+print("\twhere MCQ=correct and Open=wrong:", len(inconsistent_responses_mcq_open))
 
-inconsistent_responses_mcq_open = [el for el in inconsistent_responses if el['correct_mcq']]
-print("Number of cases where MCQ=correct and Open=wrong:", len(inconsistent_responses_mcq_open))
+inconsistent_responses_mcq_open_false = [el for el in inconsistent_responses if not el['correct_mcq'] and not el['correct']]
+print("\twhere MCQ=wrong and Open=wrong (different letters or Not match):", len(inconsistent_responses_mcq_open_false))
 
+inconsistent_responses_mcq_open_correct = [el for el in inconsistent_responses if el['correct_mcq'] and el['correct'] and el['valid_alternative']]
+print("\twhere MCQ=correct and Open=Not match (valid alternative: YES):", len(inconsistent_responses_mcq_open_correct))
+
+total_inconsistent = len(inconsistent_responses_open_mcq) + len(inconsistent_responses_mcq_open) + len(inconsistent_responses_mcq_open_false) + len(inconsistent_responses_mcq_open_correct)
+print("\tTotal:", total_inconsistent, "| Check:", total_inconsistent == len(inconsistent_responses))
+
+# for el in inconsistent_responses[:10]:
+#     print()
+#     print("Question ID:", el['question_id'])
+#     print("Gold answer:", el['gold_answer'])
+#     print("MCQ options:", el['mcq_options'])
+#     print("MCQ response:", el['mcq_response'], "| Correct MCQ?", el['correct_mcq'])
+#     print("Open response:", el['open_response'], "| Correct Open?", el['correct'])
+#     print("Justification:", el['justification'])
 
 if subset == "mmlu":
     print()
@@ -56,6 +73,16 @@ if subset == "mmlu":
         print(f"Subset: {sub} | Accuract MCQ: {round(accuracy_mcq, 1)} | Accuract Open: {round(accuracy_open, 1)}")
         print("Number of consistent responses:", round(len(consistent_responses) / len(data_sub) * 100, 1), "% |", len(consistent_responses))
         print("Number of NON-consistent responses:", round(len(inconsistent_responses) / len(data_sub) * 100, 1), "% |", len(inconsistent_responses))
+        inconsistent_responses_open_mcq = [el for el in inconsistent_responses if el['correct'] and not el['correct_mcq']]
+        print("\twhere Open=correct and MCQ=wrong:", len(inconsistent_responses_open_mcq ))
+        inconsistent_responses_mcq_open = [el for el in inconsistent_responses if el['correct_mcq'] and not el['correct']]
+        print("\twhere MCQ=correct and Open=wrong:", len(inconsistent_responses_mcq_open))
+        inconsistent_responses_mcq_open_false = [el for el in inconsistent_responses if not el['correct_mcq'] and not el['correct']]
+        print("\twhere MCQ=wrong and Open=wrong (different letters or Not match):", len(inconsistent_responses_mcq_open_false))
+        inconsistent_responses_mcq_open_correct = [el for el in inconsistent_responses if el['correct_mcq'] and el['correct'] and el['valid_alternative']]
+        print("\twhere MCQ=correct and Open=Not match (valid alternative: YES):", len(inconsistent_responses_mcq_open_correct))
+        total_inconsistent = len(inconsistent_responses_open_mcq) + len(inconsistent_responses_mcq_open) + len(inconsistent_responses_mcq_open_false) + len(inconsistent_responses_mcq_open_correct)
+        print("\tTotal:", total_inconsistent, "| Check:", total_inconsistent == len(inconsistent_responses))
         print("---------")
 
 
